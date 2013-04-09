9bee381f640ed7c5273156b620e8af833adf6736
liuqun <liuqun68@gmail.com>
2013-04-10 00:11:38 +0800

    Test Hello.java to get familiar with eclipse IDE

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+	/**
+	 * My HelloWorld app
+	 * @param args
+	 */
+	public static void main(String[] args){
+		System.out.print("Hello my friend!\n");
+		// ...
+		System.out.print("Good-bye!\n");
+	}
+}
