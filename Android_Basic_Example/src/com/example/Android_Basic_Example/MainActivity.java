16409b05a6cad5086e3027bba02863593977e438
刘群 <liuqun68@gmail.com>
2014-03-02 02:29:01 +0800

    导入安卓应用测试框架

---
+++ Android_Basic_Example/src/com/example/Android_Basic_Example/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.Android_Basic_Example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity{
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		this.setContentView(R.layout.main);
+	}
+}
